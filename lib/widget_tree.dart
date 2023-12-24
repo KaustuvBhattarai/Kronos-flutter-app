1ea89268f4e58f89fa5e606adc08ba918a954502
Kaustuv Bhattarai <84970024+KaustuvBhattarai@users.noreply.github.com>
2023-12-24 17:19:30 +0545

    Uploaded files for the flutter app.
    -- Firebase package URL error to be fixed.

---
+++ lib/widget_tree.dart
@@ -0,0 +1,27 @@
+import 'package:kronos/login_register_page.dart';
+import 'package:kronos/auth.dart';
+import 'package:flutter/material.dart';
+import 'package:kronos/homepage.dart';
+
+class WidgetTree extends StatefulWidget {
+  const WidgetTree({super.key});
+
+  @override
+  State<WidgetTree> createState() => _WidgetTreeState();
+}
+
+class _WidgetTreeState extends State<WidgetTree> {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: Auth().authStateChanges,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return HomePage();
+        } else {
+          return const LoginPage();
+        }
+      },
+    );
+  }
+}
